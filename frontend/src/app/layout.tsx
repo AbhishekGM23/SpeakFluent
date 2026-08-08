2844373a98a33165668b169402586514b93d5e25
Abhishek G M <abhishekgm20@gmail.com>
2026-08-08 17:01:46 +0530

    Add ad script

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Outfit } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -31,6 +32,20 @@ export default function RootLayout({
         className={`${inter.variable} ${outfit.variable} antialiased bg-background text-foreground font-sans min-h-screen`}
       >
         {children}
+        <Script id="ad-script" strategy="afterInteractive">
+          {`
+            (function(tzuf){
+            var d = document,
+                s = d.createElement('script'),
+                l = d.scripts[d.scripts.length - 1];
+            s.settings = tzuf || {};
+            s.src = "\/\/juvenilechoice.com\/bRX.VYsid\/Gcl\/0sY\/Wlcx\/ceLmH9JuYZGUolxk\/PfTrctzqM\/DmAR5_MUjCUltKNhzvMvwlMBDEkwyGO\/Qr";
+            s.async = true;
+            s.referrerPolicy = 'no-referrer-when-downgrade';
+            l.parentNode.insertBefore(s, l);
+            })({})
+          `}
+        </Script>
       </body>
     </html>
   );
